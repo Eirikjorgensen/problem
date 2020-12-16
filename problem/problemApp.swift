f8a6df6b9c4941620eaa3c57059aab1721041a11
Eirik Jorgensen <ej@zaptec.com>
2020-12-16 09:55:07 +0100

    problem file

---
+++ problem/problemApp.swift
@@ -0,0 +1,17 @@
+//
+//  problemApp.swift
+//  problem
+//
+//  Created by Eirik Jorgensen on 16/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct problemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
